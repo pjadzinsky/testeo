82abbbe06e0c5361bcee0d8f8f61c9de3557e781
pablo jadzinsky <pablo@vium.com>
2017-08-14 04:40:05 -0700

    first commit of log_markets.py

---
+++ my_bittrex/log_market.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import os
+import time
+import tempfile
+
+import boto3
+import hashlib
+
+from my_bittrex import volume
+import credentials      # sets environmental variables needed to test locally
+
+portfolio_csv = os.path.join('data', 'portfolio.csv')
+market_csv = os.path.join('data', 'market.csv')
+bucket_name = 'my-bittrex'
+
+s3_client = boto3.client('s3')
+
+def main():
+    # 'state' is the desired composition of our portfolio. When we 'rebalance' positions we do it
+    # to keep rations between different currencies matching those of 'state'
+    while True:
+        timestamp = int(time.time())
+        fid, filename = tempfile.mkstemp(suffix='{}.csv'.format(timestamp))
+
+        market = volume.Market()
+        last = market.summaries()['Last']
+        last['time'] = timestamp
+
+        last.to_csv(filename)
+
+        dest_key = hashlib.md5(last.to_string()).hexdigest() + "_" + str(timestamp)
+        s3_client.upload_file(filename, bucket_name, dest_key)
+
+        break
+        time.sleep(600)
+
+
+if __name__ == "__main__":
+    main()
