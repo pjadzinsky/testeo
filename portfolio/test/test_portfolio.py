cdcc30c70940db06b334573b5f2f2db4628194f8
pablo jadzinsky <pablo@vium.com>
2017-08-25 15:18:56 -0700

    Adding tests. Something is wrong. Two tests fail

--- portfolio/test/test_portfolio.py
+++ portfolio/test/test_portfolio.py
@@ -44,14 +44,13 @@ class TestPortfolio(unittest.TestCase):
 
     def test_from_currencies_4(self):
         market = self.markets.first_market()
-        currencies = "BTC,ETH,XRP,LTC,BCC"
+        currencies = "BTC,ETH,XRP,LTC"
         base = 'USDT'
         value = 10000
         p = portfolio.Portfolio.from_currencies(market, currencies, base, value)
-        print p.total_value(market, ['USDT', 'BTC'])
         print p.dataframe
 
-    def test_total_value(self):
+    def test_total_value_0(self):
         market = self.markets.first_market()
         currencies = "BTC"
         base = 'USDT'
@@ -59,6 +58,30 @@ class TestPortfolio(unittest.TestCase):
         p = portfolio.Portfolio.from_currencies(market, currencies, base, value)
         computed = p.total_value(market, ['USDT', 'BTC'])
         self.assertEqual(computed, value * (1 - portfolio.COMMISION))
+
+    def test_total_value_1(self):
+        market = self.markets.first_market()
+        currencies = "BTC,ETH,XRP,LTC"
+        base = 'USDT'
+        value = 10000
+        p = portfolio.Portfolio.from_currencies(market, currencies, base, value)
+        computed = p.total_value(market, ['USDT', 'BTC'])
+        self.assertEqual(computed, value * (1.0 - portfolio.COMMISION))
+
+    def test_value_per_currency_0(self):
+        market = self.markets.first_market()
+        currencies = "BTC,ETH,XRP"
+        base = 'USDT'
+        value = 10000
+        p = portfolio.Portfolio.from_currencies(market, currencies, base, value)
+        computed = p.value_per_currency(market, ['USDT', 'BTC'])
+
+        # computed has a row with 'USDT' that has 0 value, drop it
+        computed.drop('USDT', inplace=True)
+
+        for v in computed.values:
+            self.assertAlmostEqual(v, value * (1.0 - portfolio.COMMISION) / 3)
+        print computed
 
 if __name__ == "__main__":
     unittest.main()
