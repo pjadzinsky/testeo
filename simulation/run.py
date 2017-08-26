07d888649979a88894854a6a2a2a65ede7982583
pablo jadzinsky <pablo@vium.com>
2017-08-25 19:42:53 -0700

    Simulation working again

--- simulation/run.py
+++ simulation/run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import sys
+from collections import namedtuple
 
 import gflags
 import pandas as pd
@@ -9,12 +10,16 @@ from market import market
 from portfolio import portfolio
 
 gflags.DEFINE_multi_int('hours', [6, 12, 24, 36, 48], 'Hours in between market')
+gflags.DEFINE_multi_int('offsets', [0, 6, 12, 18], 'Hours to shift markets')
 gflags.DEFINE_float('min_percentage_change', 0.1, "Minimum variation in 'balance' needed to place an order."
                     "Should be between 0 and 1")
 gflags.DEFINE_integer('N', None, "Number of currencies to use")
 gflags.DEFINE_string('currencies', None, "comma separated list of currencies")
 FLAGS = gflags.FLAGS
 gflags.RegisterValidator('min_percentage_change', lambda x: x >=0, 'Should be positive or 0')
+
+
+Result = namedtuple('Results', ['hour', 'offset', 'rebalance', 'data'])
 
 @gflags.multi_flags_validator(['N', 'currencies'], 'One has to be defined')
 def _XOR(flags_dict):
@@ -39,7 +44,6 @@ def compare_simulations(hours, min_percentage_change, N, currencies):
         df.plot(x='time', y='value', ax=ax)
 
     plt.show()
-    print df
 
 
 def first_position(N, currencies):
@@ -67,10 +71,6 @@ def baseline(hours, N, currencies):
 def simulation(markets, min_percentage_change, N, currencies):
     # 'state' is the desired composition of our portfolio. When we 'rebalance' positions we do it
     # to keep rations between different currencies matching those of 'state'
-    if currencies:
-        state, p = portfolio.Portfolio.from_currencies(markets.first_market(), currencies, base, value)
-    elif N:
-        state, p = portfolio.Portfolio.from_largest_markes(markets.first_market(), N, base, value)
 
     total_values = []
     times = []
@@ -83,6 +83,19 @@ def simulation(markets, min_percentage_change, N, currencies):
     return df
 
 
+def simulate(hour, offset, markets, state, p, rebalance):
+    times = []
+    values = []
+    for current_time, current_market in markets:
+        times.append(current_time)
+        if rebalance:
+            p.rebalance(current_market, state, ['BTC'], FLAGS.min_percentage_change)
+
+        values.append(p.total_value(current_market, ['USDT', 'BTC']))
+
+    data = pd.DataFrame({'time': times, 'value': values})
+    return Result(hour, offset, rebalance, data)
+
 if __name__ == "__main__":
     try:
         argv = FLAGS(sys.argv)
@@ -90,5 +103,33 @@ if __name__ == "__main__":
         print "%s\nUsage: %s ARGS\n%s" % (e, sys.argv[0], FLAGS)
         sys.exit(1)
 
-    markets = market.Markets(3600 * 24, 0)
-    print simulation(markets, 0, FLAGS.N, FLAGS.currencies)
+    results = []
+
+    # do only one simulation without rebalancing as a baseline
+    hour = min(FLAGS.hours)
+    offset = 0
+    markets = market.Markets(3600 * hour, 0)
+    if FLAGS.currencies:
+        state, p = portfolio.Portfolio.from_currencies(markets.first_market(), FLAGS.currencies, base, value)
+    results.append(simulate(hour, offset, markets, state, p, False))
+
+    for offset in FLAGS.offsets:
+        for hour in FLAGS.hours:
+            markets = market.Markets(3600 * hour, 3600 * offset)
+
+            if FLAGS.currencies:
+                state, p = portfolio.Portfolio.from_currencies(markets.first_market(), FLAGS.currencies, base, value)
+            elif FLAGS.N:
+                state, p = portfolio.Portfolio.from_largest_markes(markets.first_market(), FLAGS.N, base, value)
+
+
+            rebalance = True
+            results.append(simulate(hour, offset, markets, state, p, rebalance))
+
+    fig, ax = plt.subplots()
+    for r in results:
+        ax.plot(r.data['time'], r.data['value'], label="{}_{}_{}".format(r.hour, r.offset, r.rebalance))
+
+    ax.legend(loc=2)
+    plt.show()
+
