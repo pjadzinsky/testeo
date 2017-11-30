bb572ad759a1426ba221d56e8a8338a8955fc7fc
Pablo Jadzinsky <pablo@opas.ai>
2017-11-29 23:49:08 -0800

    Removing unused methods Adding method to compute distance in time in between uploaded files with timestamps in the key Added method to put_csv in s3 Changed name from update_csv to append_to_csv to more closely reflect effect

--- s3_utils.py
+++ s3_utils.py
@@ -1,6 +1,7 @@
 import json
 import os
 import tempfile
+import re
 
 import pandas as pd
 
@@ -14,7 +15,7 @@ def log_df(bucket_name, s3_key, some_df):
         bucket.upload_file(filename, s3_key)
 
 
-def update_csv(other, bucket_name, s3_key_suffix):
+def append_to_csv(other, bucket_name, s3_key, save_index=False):
     """
     download the csv associated with bucket/s3_key (or create an empty one if bucket/s3_key is empty)
     add the contents of row and save it back into bucket/s3_key  
@@ -25,13 +26,14 @@ def update_csv(other, bucket_name, s3_key_suffix):
     """
     _, temp = tempfile.mkstemp()
     bucket = config.s3_client.Bucket(bucket_name)
-    s3_key_suffix = s3_key_suffix.rstrip('.csv')    # just in case, ".csv" is added below again
-    s3_key = '{account}/{suffix}.csv'.format(account=os.environ['BITTREX_ACCOUNT'],
-                                             suffix=s3_key_suffix)
+
     object = bucket.Object(s3_key)
     try:
         object.download_file(temp)
-        df = pd.read_csv(temp, comment='#')
+        if save_index:
+            df = pd.read_csv(temp, index_col=0, comment='#')
+        else:
+            df = pd.read_csv(temp, comment='#')
     except:
         df = pd.DataFrame([])
 
@@ -40,14 +42,28 @@ def update_csv(other, bucket_name, s3_key_suffix):
 
     df = df.append(other)
     if os.environ['PORTFOLIO_REPORT']:
-        df.to_csv(temp, index=False)
+        df.to_csv(temp, index=save_index)
         bucket.upload_file(temp, s3_key)
     return df
 
 
+def put_csv(df, bucket_name, s3_key, **kwargs):
+    """
+    Put dataframe from csv into bucketname and s3_key
+    :param bucket_name: 
+    :param s3_key: 
+    :return: 
+    """
+    bucket = config.s3_client.Bucket(bucket_name)
+
+    _, temp = tempfile.mkstemp()
+    df.to_csv(temp, **kwargs)
+    bucket.upload_file(temp, s3_key)
+
+
 def get_df(bucket_name, s3_key, **read_csv_kwarg):
     """
-    Get dataframe from csv associated with bucketname and s3_key_suffix and return it
+    Get dataframe from csv associated with bucketname and s3_key and return it
     If any problem returns an empty DF
     :param bucket_name: 
     :param s3_key: 
@@ -67,37 +83,26 @@ def get_df(bucket_name, s3_key, **read_csv_kwarg):
     return df
 
 
-def get_json_contents(bucket_name, s3_key_suffix):
+def bucket_timestamps(bucket_name):
     """
-    Get python object stored in json format associated with bucketname and s3_key_suffix and return it
     
     :param bucket_name: 
-    :param s3_key_suffix: 
     :return: 
+    return all timestamps in the given bucket/<account>/<time_stamp>_suffix
+
     """
     bucket = config.s3_client.Bucket(bucket_name)
-    s3_key = "{account}/{s3_key_suffix}".format(account=os.environ['BITTREX_ACCOUNT'],
-                                                s3_key_suffix=s3_key_suffix)
+    all_summaries = bucket.objects.all()
+    s3_keys = [summary.key for summary in all_summaries]
 
-    object = bucket.Object(s3_key)
-    _, temp = tempfile.mkstemp()
-    try:
-        object.download_file(temp)
-        return json.load(temp)
-    except:
-        return None
+    # limit s3_keys to current account and those with digits on them
+    s3_keys = [key for key in s3_keys if os.environ['BITTREX_ACCOUNT'] in key]
+    print s3_keys
+    regex = re.compile('.*\d+')
+    s3_keys = [key for key in s3_keys if regex.match(key)]
+    print s3_keys
 
+    regex = re.compile('\d+')
+    times = [int(regex.findall(s3_key)[0]) for s3_key in s3_keys]
+    return times
 
-def update_json(object, bucket_name, s3_key_suffix):
-    """
-    store 'object' as json under bucket_name and <account>/s3_key_suffix
-    """
-    bucket = config.s3_client.Bucket(bucket_name)
-    s3_key_suffix = s3_key_suffix.rstrip('.json')    # just in case, ".json" is added below again
-    s3_key = '{account}/{suffix}.json'.format(account=os.environ['BITTREX_ACCOUNT'],
-                                              suffix=s3_key_suffix)
-
-    if os.environ['PORTFOLIO_REPORT']:
-        _, temp = tempfile.mkstemp()
-        json.dump(object, temp)
-        bucket.upload_file(temp, s3_key)
